52beef5454ad83a44c39cbe55fe17d046a1acf2c
NOBODY1011986 <nobody1011986@gmail.com>
2025-04-15 02:06:20 +0300

    finaly OK with database | almost dome with prodPage

--- js/fill_product_card.js
+++ js/fill_product_card.js
@@ -9,7 +9,6 @@ fetch('../database/base.xml')
 
         const link = new URL(window.location.href);
         const prod_id = link.searchParams.get('id');
-        
         let gun; let found = false;
         for (let i = 0; i < guns.length; i++) {
             if (prod_id == guns[i].getElementsByTagName('prod_code')[0].textContent) {
@@ -24,93 +23,118 @@ fetch('../database/base.xml')
         document.querySelector('main>h1').textContent = guns[gun].getElementsByTagName('name')[0].textContent;
         document.querySelector('p#installment').textContent = guns[gun].getElementsByTagName('installment')[0].textContent;
         
+        let avalibility_line = document.querySelector('span.avalibility');
+        avalibility_line.textContent = guns[gun].getElementsByTagName('avalibility')[0].textContent;
+        if (guns[gun].getElementsByTagName('avalibility')[0].textContent != "В наличии"){
+            avalibility_line.style.color = 'red';
+        }
+        
+        //work with table
+        const tags = [
+        'type',
+        'caliber',
+        'barrel_type',
+        'shot_speed',
+        'prototype',
+        'sight_mount',
+        'under_barrel_mount',
+        'energy_source',
+        'shooting_mode',
+        'ammunition',
+        'magazine_capacity',
+        'blowback',
+        'bolt',
+        'barrel_delay',
+        'form_factor',
+        'safety_lock',
+        'power',
+        'charge_count',
+        'sight',
+        'magazine_type',
+        'trigger_mechanism',
+        'weight',
+        'overall_length',
+        'barrel_length',
+        'dimensions',
+        'body_material',
+        'barrel_material',
+        'color',
+        'manufacturer',
+        'country',
+        'warranty',
+        ];
 
-        document.getElementsByTagName('tbody')[0].innerHTML = `
-            <tr>
-                <td>Тип</td>
-                <td>${guns[gun].getElementsByTagName('type')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Калибр, мм</td>
-                <td>${guns[gun].getElementsByTagName('caliber')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Скорость выстрела, м/с</td>
-                <td>${guns[gun].getElementsByTagName('shot_speed')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Боевой прототип</td>
-                <td>${guns[gun].getElementsByTagName('prototype')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Тип ствола</td>
-                <td>${guns[gun].getElementsByTagName('sight_mount')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Крепление для прицела</td>
-                <td>${guns[gun].getElementsByTagName('under_barrel_mount')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Подствольное крепление</td>
-                <td>${guns[gun].getElementsByTagName('under_barrel_mount')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Источник энергии</td>
-                <td>${guns[gun].getElementsByTagName('energy_source')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Режим стрельбы</td>
-                <td>${guns[gun].getElementsByTagName('shooting_mode')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Боеприпасы</td>
-                <td>${guns[gun].getElementsByTagName('ammunition')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Емкость магазина</td>
-                <td>${guns[gun].getElementsByTagName('magazine_capacity')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Блоубэк</td>
-                <td>${guns[gun].getElementsByTagName('blowback')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Затвор</td>
-                <td>${guns[gun].getElementsByTagName('bolt')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Ствольная задержка</td>
-                <td>${guns[gun].getElementsByTagName('form_factor')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Форма-фактор</td>
-                <td>${guns[gun].getElementsByTagName('weight')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Предохранитель</td>
-                <td>${guns[gun].getElementsByTagName('overall_length')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Мощность</td>
-                <td>${guns[gun].getElementsByTagName('body_material')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>По количеству зарядов</td>
-                <td>${guns[gun].getElementsByTagName('type')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Прицел</td>
-                <td>${guns[gun].getElementsByTagName('type')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>Тип магазина</td>
-                <td>${guns[gun].getElementsByTagName('type')[0].textContent}</td>
-            </tr>
-            <tr>
-                <td>УСМ</td>
-                <td>${guns[gun].getElementsByTagName('type')[0].textContent}</td>
-            </tr>
-        `
+        const names = [
+        'Тип',
+        'Калибр, мм',
+        'Тип ствола',
+        'Скорость выстрела, м/с',
+        'Боевой прототип',
+        'Крепление для прицела',
+        'Подствольное крепление',
+        'Источник энергии',
+        'Режим стрельбы',
+        'Боеприпасы',
+        'Емкость магазина',
+        'Блоубэк',
+        'Затвор',
+        'Ствольная задержка',
+        'Форм-фактор',
+        'Предохранитель',
+        'Мощность',
+        'По количеству зарядов',
+        'Прицел',
+        'Тип магазина',
+        'УСМ',
+        'Вес, кг',
+        'Общая длина, мм',
+        'Длина ствола, см',
+        'Габаритные размеры',
+        'Материал корпуса',
+        'Материал ствола',
+        'Цвет',
+        'Производитель',
+        'Страна производства',
+        'Гарантия, месяцев',
+        ];
+
+        function fill_table_block(begin, end, block_num){
+            let table = ''; let content;
+            for (let i = begin; i < end; i++) {
+                content = guns[gun].getElementsByTagName(tags[i])[0];
+                if (content){
+                    table += `
+                    <tr>
+                        <td>${names[i]}</td>
+                        <td>${content.textContent}</td>
+                    </tr>
+                    `;
+                }
+            }
+            document.getElementsByTagName('tbody')[block_num].innerHTML = table;
+        }
+
+        fill_table_block(0, 21, 0);
+        fill_table_block(21, 24, 1);
+        fill_table_block(24, 28, 2);
+        fill_table_block(28, 31, 3);
+
+
+        //images
+        document.querySelector('img.general').src = guns[gun].getElementsByTagName('images')[0].getElementsByTagName('image')[0].textContent;
+        imgs = document.getElementsByClassName('galery')[0];
+        imgs_links = guns[gun].getElementsByTagName('image');
+        imgs.innerHTML = '';
+        for (let i = 1; i < imgs_links.length; i++) {
+            imgs.innerHTML += `
+            <div class="img-cont">
+                <img src="${imgs_links[i].textContent}" class="general">
+            </div>`;
+        }
+
+
+
+
         }
         else{
             alert('Товара с таким ID в базе не обнаружено');
